178a6a0f769bb60d8b922ab35aec717ccb1fa850
YASH SRI <divyanshrajshrivastava@gmail.com>
2025-12-31 01:54:18 +0530

    FIX_api_link_2

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-import { API_BASE } from "@/config";
+// import { API_BASE } from "@/config";
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
